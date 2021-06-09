25fd379612ab8c72c171987e5ec2beab54115165
Daniel García <daniel.garciaa@autonoma.edu.co>
2021-06-08 21:05:28 -0500

    Email model to user

--- app/Http/Controllers/Api/TasksController.php
+++ app/Http/Controllers/Api/TasksController.php
@@ -11,10 +11,10 @@ class TasksController extends Controller
 {
     public function getAllTasks(Request $request)
     {
-        $client = new ClientHttp('');        
+        $client = new ClientHttp('');
         $current_date = Carbon::now();
         $first_date = $current_date->firstOfMonth()->toDateString();
-        $end_date = $current_date->lastOfMonth()->toDateString();        
+        $end_date = $current_date->lastOfMonth()->toDateString();
 
         $response = $client->getStrict('management/tasks', [
             'id_user' => $request->user_id,
@@ -27,13 +27,10 @@ class TasksController extends Controller
         $in_progress = [];
         $completed = [];
 
-        if($response['status'] == "success")
-        {
+        if ($response['status'] == "success") {
             unset($response['status']);
-            foreach ($response as $task) 
-            {
-                switch ($task['management_status_id']) 
-                {
+            foreach ($response as $task) {
+                switch ($task['management_status_id']) {
                     case '1':
                         $task = $this->addCalendarData($task);
                         array_push($expired, $task);
@@ -48,7 +45,7 @@ class TasksController extends Controller
                         $task = $this->addCalendarData($task);
                         array_push($completed, $task);
                         break;
-                    
+
                     default:
                         # code...
                         break;
@@ -57,7 +54,7 @@ class TasksController extends Controller
         }
 
         return response()->json([
-            'expired' => $expired,            
+            'expired' => $expired,
             'in_progress' => $in_progress,
             'completed' => $completed
         ], 200);
@@ -67,7 +64,7 @@ class TasksController extends Controller
     public function createTask(Request $request)
     {
         $client = new ClientHttp('');
-        
+
         // crear una nueva tarea en la base de datos.
         $task_information = $request->all();
         $task_information['id_user'] = $request->id_user;
@@ -78,9 +75,13 @@ class TasksController extends Controller
         $properties = $request->properties;
         $property = $client->get('property/get/' . $properties[0]);
         $for_sale = $property['for_sale']; //Esto creo que esta sacando un string pero deberia ser booleano
+        $sale_value = ($for_sale === 'true');
 
 
         $user = $client->get('user/get/' . $request->id_user);
+        $name = $user['first_name'];
+        $lastname = $user['last_name'];
+        $fechaVisita = $request->date;
 
         return response()->json([
             'code' => $for_sale,
@@ -88,15 +89,25 @@ class TasksController extends Controller
             // 'pruebas' => $filters
         ], 200);
 
-        if($response == 200)
-        {
+        if ($response == 200) {
             //SEND EMAIL
             $details = [
-                'title' => 'Probando la app de vista',
-                'body' => 'jejejejejeje'
+                'title' => 'Señor(a) ' . $name . ' ' . $lastname,
+                'body' => 'De acuerdo a nuestra conversación, le informamos que el día ' . $fechaVisita . ' visitaremos el inmueble en mención ' . ' con el cliente ' . $name . ' ' . $lastname . '\n' . '{ficha} el inmueble tiene adjunto una ficha, se debe enviar en ese correo como adjunto en pdf. \n Por favor tener en cuenta que cualquier acercamiento y negociación deberá hacerse con nuestro conocimiento y participación. En caso de cerrarse el negocio, la remuneración de la inmobiliaria por la administración del inmueble es el 10% más IVA del mismo porcentaje. \n Cordial saludo, \n
+                Nombre del agente y de la empresa',
             ];
-            
-            \Mail::to('alejogomes2339-12@hotmail.com')->send(new \App\Mail\ClientMail($details));
+
+            $details2 = [
+                'title' => 'Señor(a) ' . $name . ' ' . $lastname,
+                'body' => 'De acuerdo a nuestra conversación, le informamos que el día ' . $fechaVisita . ' visitaremos el inmueble en mención ' . ' con el cliente ' . $name . ' ' . $lastname . '\n' . '{ficha} el inmueble tiene adjunto una ficha, se debe enviar en ese correo como adjunto en pdf. \n Por favor tener en cuenta que cualquier acercamiento y negociación deberá hacerse con nuestro conocimiento y participación. En caso de cerrarse el negocio, la remuneración de la inmobiliaria por la administración del inmueble es el 3% más IVA del mismo porcentaje. \n Cordial saludo, \n
+                Nombre del agente y de la empresa',
+            ];
+
+            if ($sale_value) {
+                \Mail::to('danipipe1998@hotmail.com')->send(new \App\Mail\ClientMail($details));
+            } else {
+                \Mail::to('danipipe1998@hotmail.com')->send(new \App\Mail\ClientMail($details2));
+            }
         }
 
         return response()->json([
--- resources/views/client-mail.blade.php
+++ resources/views/client-mail.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Vista Inmobiliaria</title>
 </head>
+
 <body>
     <h1>{{ $details['title'] }}</h1>
     <p>{{ $details['body'] }}</p>
-   
+
     <p>Gracias!</p>
 </body>
+
 </html>
